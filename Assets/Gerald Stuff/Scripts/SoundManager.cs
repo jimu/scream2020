a7f5bf81df8b51fee09015464ebb29e356df143c
Gerald Clark <farthestempire@gmail.com>
2020-10-13 22:24:28 -0500

    Added super simple Sound effect to make sure shit works

---
+++ Assets/Gerald Stuff/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager mainAudio;
+
+    [SerializeField]
+    private string bulletFire;
+
+    public FMOD.Studio.EventInstance bulletFireEvent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (mainAudio != null)
+        {
+            DestroyImmediate(this);
+        }
+        else
+            mainAudio = this;
+        DontDestroyOnLoad(gameObject);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Key Pressed");
+            FMODUnity.RuntimeManager.PlayOneShot(bulletFire);
+        }
+    }
+}
