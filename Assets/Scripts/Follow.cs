038c53ff5e64c31ae1f80836ace823461082a065
Jim Urbas <git@jimu.net>
2020-10-14 04:55:40 -0500

    Add minimap and clock

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Follow : MonoBehaviour
+{
+
+    public Transform target;
+    [SerializeField] float height = 10f;
+    [SerializeField] Vector3 offset = new Vector3(-1f, 12f, -4f);
+
+    private void Update()
+    {
+        Vector3 dest = target.position + offset;
+        dest.x++;
+        dest.y = height;
+        dest.z++;
+        transform.position = target.position + offset;
+        transform.LookAt(target);
+    }
+}
