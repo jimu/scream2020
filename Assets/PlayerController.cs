215cc3714602da799fecbc10f6c770f06768cc83
Jim Urbas <git@jimu.net>
2020-10-14 23:19:33 -0500

    Add playercontroller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float playerSpeed = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+        float deltaZ = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        transform.Translate(deltaX, 0f, deltaZ);
+    }
+}
