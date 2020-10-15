00bbd53e51ecead3373eb11aa237d492db3cdf22
Jim Urbas <git@jimu.net>
2020-10-15 00:28:16 -0500

    add debug camera box

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -3,17 +3,13 @@ using UnityEngine.AI;
 
 public class Follow : MonoBehaviour
 {
-
     public Transform target;
-    [SerializeField] float height = 10f;
+    public int mode;
+
     [SerializeField] Vector3 offset = new Vector3(-1f, 12f, -4f);
 
     private void Update()
     {
-        Vector3 dest = target.position + offset;
-        dest.x++;
-        dest.y = height;
-        dest.z++;
         transform.position = target.position + offset;
         transform.LookAt(target);
     }
