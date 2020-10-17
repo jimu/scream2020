d5198fbabb5dac1f939e462543f6f16ef27ab62e
Jim Urbas <git@jimu.net>
2020-10-17 07:01:45 -0500

    Add player marker to minimap

--- Assets/Scripts/MiniMap2.cs
+++ Assets/Scripts/MiniMap2.cs
@@ -7,8 +7,11 @@ using UnityEngine;
 public class MiniMap2 : MonoBehaviour
 {
     [SerializeField] GameObject prefabX;
+    [SerializeField] GameObject prefabPlayerMarker;
     List<Enemy> enemies;
     RectTransform[] poolX;
+    RectTransform playerMarker;
+    Transform player;
 
     public void Init(List<Enemy> enemies)
     {
@@ -16,6 +19,9 @@ public class MiniMap2 : MonoBehaviour
         poolX = new RectTransform[enemies.Count];
         for(int i = 0; i< enemies.Count; ++i)
             poolX[i] = Instantiate(prefabX, transform).GetComponent<RectTransform>();
+
+        playerMarker = Instantiate(prefabPlayerMarker, transform).GetComponent<RectTransform>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -30,5 +36,6 @@ public class MiniMap2 : MonoBehaviour
             poolX[i].anchoredPosition = new Vector2(pos.x + 5 , pos.z);
             i++;
         }
+        playerMarker.anchoredPosition = new Vector2(player.position.x + 5, player.position.z);
     }
 }
