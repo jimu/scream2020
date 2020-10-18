120bc74a2ffa8900e01111fa28572b469a609cc9
Jim Urbas <git@jimu.net>
2020-10-17 19:55:26 -0500

    Add Fallen branches/trees block path

---
+++ Assets/Scripts/Branch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Branch : MonoBehaviour
+{
+    bool isBlocking = false;
+    [Header("Branch falls north by default.  To cross path, Adjust Y or rotate in scene")]
+    [SerializeField] Vector3 blockingAngle = new Vector3(90, 0, 0);
+    [SerializeField] AudioClip sfx;
+
+    // get/set branch blocking the path
+    public void Block()
+    {
+        isBlocking = true;
+        transform.rotation = isBlocking ? Quaternion.Euler(blockingAngle): Quaternion.identity;
+        GetComponent<NavMeshObstacle>().enabled = true;
+        GameManager.instance.GetComponent<AudioSource>().PlayOneShot(sfx);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,19 +26,41 @@ public class PlayerController : MonoBehaviour
         transform.Translate(deltaX, 0f, deltaZ);
 
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
-            Attack();
+            Interact();
         if (Input.GetKeyDown(KeyCode.Q))
             abilities[0].TriggerAbility();
     }
+    void Interact()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+        {
+            GameObject o = hit.collider.gameObject;
+            if (o.CompareTag("Enemy"))
+                o.GetComponent<Enemy>().Damage(1);
+            else if (o.CompareTag("Branch"))
+                o.GetComponent<Branch>().Block();
+            else
+                AttackMissed();
+        }
+    }
 
-    void Attack()
+    void AttackMissed()
+    {
+        Vector3 markPos = transform.position;
+        markPos.y = 0.01f;
+        Instantiate(clawMarkPrefab, markPos, Quaternion.Euler(90, 0, 0));
+        GameManager.instance.GetComponent<AudioSource>().PlayOneShot(attackSounds[attackSoundIndex]);
+        attackSoundIndex = (attackSoundIndex + 1) % attackSounds.Length;
+    }
+    void AttackDepricated(Enemy enemy)
     {
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity) && hit.collider.gameObject.CompareTag("Enemy"))
         {
             GameObject o = hit.collider.gameObject;
-            Enemy enemy = o.GetComponent<Enemy>();
+            Enemy enemyx = o.GetComponent<Enemy>();
             enemy.Damage(1);
         }
         else
