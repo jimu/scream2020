9e0892dbde64886cf690d51e2dbc3fb6ed82fd10
Jim Urbas <git@jimu.net>
2020-10-20 06:16:56 -0500

    Fix TerrorMeter issue (rendering behind trees)

--- Assets/Scripts/TerrorMeter.cs
+++ Assets/Scripts/TerrorMeter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 #pragma warning disable CS0649
 
@@ -19,7 +20,7 @@ public class TerrorMeter : MonoBehaviour
     public void Set(int terrorLevel)
     {
         for (int i = 0; i < icons.Length; ++i)
-            icons[i].GetComponent<SpriteRenderer>().sprite = i >= terrorLevel ? goodSprite : badSprite;
+            icons[i].GetComponent<Image>().sprite = i >= terrorLevel ? goodSprite : badSprite;
 
     }
 }
