6481f4d6b654626c10d09913c84aadb5173a328b
BunnyViking <tenabraex@gmail.com>
2020-10-21 19:08:43 +1100

    various
    title screen
    
    new dirt texture
    
    visual tweaks to scene3

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("scene3");
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
