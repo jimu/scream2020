a1e2e7d9b72e9984ad31b901aec19d8ba184c872
Jim Urbas <git@jimu.net>
2020-10-21 22:51:28 -0500

    Animate moon to indicate time remaining in scene

---
+++ Assets/Scripts/MoveMoon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMoon : MonoBehaviour
+{
+    [Tooltip("Starting position of moon")]
+    [SerializeField] Transform startPosition;
+
+    [Tooltip("Ending position of moon")]
+    [SerializeField] Transform endPosition;
+    
+    [Header("Duration of scene in seconds")]
+    [SerializeField] float duration;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = startPosition.position;
+        endPos = endPosition.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(startPos, endPos, Time.time / duration);
+    }
+}
