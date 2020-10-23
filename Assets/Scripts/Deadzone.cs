83bbb2188c7c0aaaffaca0c08db344b4debc2572
Jim Urbas <git@jimu.net>
2020-10-23 08:50:26 -0500

    Deadzone

---
+++ Assets/Scripts/Deadzone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deadzone : MonoBehaviour
+{
+    PlayerController player;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string tag = other.gameObject.tag;
+        Debug.Log("******* Add " + other.gameObject.name + " (" + tag + ")");
+
+        if (tag == "Enemy" || tag == "Corpse" || tag == "Loot" || tag == "Log")
+        {
+            Debug.Log("******* Add " + other.gameObject.name + " (" + tag + ")");
+            //nearbyThings.Add(other.gameObject);
+            //FindClosestThing();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        string tag = other.gameObject.tag;
+        Debug.Log("****** Remove " + other.gameObject.name + " (" + tag + ")");
+
+        if (tag == "Enemy" || tag == "Corpse" || tag == "Loot" || tag == "Log")
+        {
+            Debug.Log("****** Remove " + other.gameObject.name + " (" + tag + ")");
+            //nearbyThings.Remove(other.gameObject);
+            //FindClosestThing();
+        }
+    }
+}
